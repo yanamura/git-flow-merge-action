bcf79db7c2fa62990371fc5752001e595914e96d
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-20 00:49:05 +0900

    add merge

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {context, GitHub} from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const branch: string = core.getInput('branch')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = new GitHub(core.getInput('github_token'))
 
-    core.setOutput('time', new Date().toTimeString())
+    await octokit.repos.merge({
+      ...context.repo,
+      base: 'master',
+      head: branch
+    })
+
+    await octokit.repos.merge({
+      ...context.repo,
+      base: 'develop',
+      head: branch
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
