2d18c9883763b7ff0e1758995ff300097c35ce5d
Yasuharu Yanamura <yanamura3@gmail.com>
2020-05-29 16:09:26 +0900

    update: use merge & addTag method

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,34 @@
 import * as core from '@actions/core'
 import {context, GitHub} from '@actions/github'
 
+const octokit = new GitHub(core.getInput('github_token'))
+
+async function merge(branch: string, to: string): Promise<string> {
+  const response = await octokit.repos.merge({
+    ...context.repo,
+    base: to,
+    head: branch
+  })
+  const newMasterSha = response.data.sha
+  core.info(`sha = ${newMasterSha}`)
+  return newMasterSha
+}
+
+async function addTag(tag: string, sha: string): Promise<void> {
+  await octokit.git.createRef({
+    ...context.repo,
+    ref: `refs/tags/${tag}`,
+    sha
+  })
+}
+
 async function run(): Promise<void> {
   const branch: string = core.getInput('branch')
   core.info(`branch name=${branch}`)
 
-  const octokit = new GitHub(core.getInput('github_token'))
-
   let newMasterSha = ''
   try {
-    const response = await octokit.repos.merge({
-      ...context.repo,
-      base: 'master',
-      head: branch
-    })
-    newMasterSha = response.data.sha
-    core.info(`sha = ${newMasterSha}`)
+    newMasterSha = await merge(branch, 'master')
   } catch (error) {
     core.setFailed(`master merge failed::${error.message}`)
   }
@@ -24,22 +37,14 @@ async function run(): Promise<void> {
   if (tag) {
     core.info(`tag name=${tag}`)
     try {
-      await octokit.git.createRef({
-        ...context.repo,
-        ref: `refs/tags/${tag}`,
-        sha: newMasterSha
-      })
+      await addTag(tag, newMasterSha)
     } catch (error) {
       core.setFailed(`add tag failed::${error.message}`)
     }
   }
 
   try {
-    await octokit.repos.merge({
-      ...context.repo,
-      base: 'develop',
-      head: branch
-    })
+    await merge(branch, 'develop')
   } catch (error) {
     core.setFailed(`develop merge failed::${error.message}`)
   }
